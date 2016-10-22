0721d6455b11569aa1e513591f7aafd85650124f
Rémy GIBERT <burniouf@users.noreply.github.com>
2016-10-22 23:01:07 +0200

    Kernel version 0.8 : Docs...

--- _Tools/docgen.cmd
+++ _Tools/docgen.cmd
@@ -39,7 +39,8 @@ for /f %%F in ('dir /b /ogn %~dp0..\SYS\KERNEL.S*.txt') do (
      if "!line:~0,4!" EQU "*   " set LINE= + !LINE:~4!
      if "!line:~0,3!" EQU "*  " set LINE=+ !LINE:~3!
      if "!line:~0,2!" EQU "* " set LINE=!LINE:~2!
-     echo !LINE!  >>!DOCFILE!
+	 if "!line:~0,1!" EQU "#" echo.>>!DOCFILE!
+     (echo !LINE!)>>!DOCFILE!
      )
     )
    )
